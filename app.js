d98fa24ad32dfff33577f133e08830c6af0690bc
JacobWylie <jacobwylie@gmail.com>
2017-07-11 16:13:25 +0200

    database setup, root and index routes created. blog posts added to index via ejs loop

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+///////////////////////////
 // Dependencies
+///////////////////////////
+
 const express    = require('express'),
 	  app 	     = express(),
 	  bodyParser = require('body-parser'),
 	  mongoose   = require('mongoose');
+
+
+///////////////////////////
+//	MIDDLEWARE
+///////////////////////////
 
 // Connect to local db
 mongoose.connect('mongodb://localhost/restful_blog', {useMongoClient: true});
@@ -13,12 +21,73 @@ app.use(express.static('public'));
 // Parse data through POST forms
 app.use(bodyParser.urlencoded({extended: true}));
 
-// title
-// image
-// body
-// created
 
-// Local dev server
+///////////////////////////
+// MONGOOSE/MODEL CONFIG
+///////////////////////////
+
+// Mongodb database schema
+const blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+// Compile schema into mongoose model
+const Blog = mongoose.model('Blog', blogSchema);
+
+
+///////////////////////////
+// RESTFUL ROUTES
+///////////////////////////
+
+// ROOT -> INDEX
+app.get('/', (req, res) => {
+	res.redirect('/blogs');
+})
+
+// INDEX - 
+app.get('/blogs', (req, res) => {
+	Blog.find({}, (err, blogs) => {
+		if(err) {
+			console.log('error')
+		} else {
+			res.render('index', {blogs: blogs});
+		}
+	})
+
+})
+
+
+///////////////////////////
+// LOCAL DEV SERVER
+///////////////////////////
+
 app.listen(3000, () => {
 	console.log('App running on localhost:3000');
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
